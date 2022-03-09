e45cb7759e8a6783d7e45a2a3ee15b4a8c608c27
simon-lb <89006540+simon-lb@users.noreply.github.com>
2022-03-09 15:06:58 +0200

    Update system_info_light.py

--- system_info_light.py
+++ system_info_light.py
@@ -15,6 +15,8 @@ Created on Fri Jul  2 18:17:18 2021
 #
 # Proper way of running this script:
 # python system_info_light.py "C:\\Users\\sgolan\\Desktop\\hosts_system_info_report.txt" "C:\\Users\\sgolan\\Desktop\\hosts_path.txt"
+# run on linux: pip3 install paramiko argparse
+# python3 system_info_light.py hosts_system_info_report.txt hosts_path.txt
 
 import paramiko # ssh library
 import argparse # argumets parsing library
@@ -115,7 +117,7 @@ def main():
                     f.write("SYSTEM INFO FOR HOST:\n" + str(get_hostname(ssh)) + "IP address: " + str(host) + "\n" + "System Time: " + str(get_time(ssh)) + "\n\n" + "CPU Model: \n" +
                                       str(get_cpu(ssh)) + "\n\n" + "Number of CPU's: \n" + str(get_cpu_count(ssh)) + "\n\n" +
                                       "Installed NIC Driver:\n" + str(get_nic(ssh)) + "\n\n" +
-                                      "Installed Drivers:\n" + str(get_drivers(ssh)) + "\n\n" + ""END OF REPORT FOR THIS HOST " + ' > '*30 + " NEXT HOST..." + "\n\n")
+                                      "Installed Drivers:\n" + str(get_drivers(ssh)) + "\n\n" + "END OF REPORT FOR THIS HOST " + ' > '*30 + " NEXT HOST..." + "\n\n")
                         
                     
                     #print to console that file was created with file path
